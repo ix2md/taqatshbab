a2f1d7bc84378c2d4064b60e4d24a6684b091528
ix2md <ix2mdalattas@gmail.com>
2026-02-17 17:10:44 +0300

    Update decorations: img/2.png top right, img/1.png top left

--- deploy_final.bat
+++ deploy_final.bat
@@ -1,10 +1,10 @@
 @echo off
 "C:\Program Files\Git\cmd\git.exe" checkout main
 "C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Fix watermark positioning and image generation"
+"C:\Program Files\Git\cmd\git.exe" commit -m "Update decorations: img/2.png top right, img/1.png top left"
 "C:\Program Files\Git\cmd\git.exe" push origin main
 "C:\Program Files\Git\cmd\git.exe" checkout gh-pages
-"C:\Program Files\Git\cmd\git.exe" merge main -m "Merge watermark fix from main"
+"C:\Program Files\Git\cmd\git.exe" merge main -m "Merge decorations update"
 "C:\Program Files\Git\cmd\git.exe" push origin gh-pages
 "C:\Program Files\Git\cmd\git.exe" checkout main
-del deploy_watermark_fix.bat
+del deploy_final.bat
