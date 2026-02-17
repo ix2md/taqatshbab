22a03bc77a537eefeee4c320629683747b50d5c4
ix2md <ix2mdalattas@gmail.com>
2026-02-17 13:02:59 +0300

    Cleanup temporary batch files

--- deploy_gh_pages.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"C:\Program Files\Git\cmd\git.exe" checkout -b gh-pages
-if %errorlevel% neq 0 (
-    echo Branch gh-pages might already exist, switching to it...
-    "C:\Program Files\Git\cmd\git.exe" checkout gh-pages
-)
-"C:\Program Files\Git\cmd\git.exe" push -u origin gh-pages
